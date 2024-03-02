d6ac2e92e2fa1e737708744e27f6c6e67fa97d41
Ajay Kaloty <kalotya99@gmail.com>
2024-03-02 15:06:08 -0800

    Changed the names of the prompt questions

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const generateSvg = ({})
 inquirer.prompt([
     {
         type: 'input',
-        name: 'logo-text',
+        name: 'logoText',
         message: 'Enter the 3 letters you want for your logo.'
     },
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'What color do you want the logo text to be?'
     },
     {
         type: 'list',
-        name: 'logo-shape',
+        name: 'logoShape',
         choices: ['Circle', 'Triangle', 'Square'],
         message: 'What shape do you want your logo to be?'
     },
     {
         type: 'input',
-        name: 'shape-color',
+        name: 'shapeColor',
         message: 'What color do you want this shape to be? (this will be different from the text color!)'
     },
 ])
