a66029cd141483cd55974a87ae382583532ac3f6
Ajay Kaloty <kalotya99@gmail.com>
2024-03-02 16:16:58 -0800

    Added let method + Simplified the super method

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
-const fs = require("fs")
+const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 
-const generateSvg = ({})
-
-
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type: 'input',
-        name: 'logoText',
-        message: 'Enter the 3 letters you want for your logo.'
+      type: "input",
+      name: "logoText",
+      message: "Enter the 3 letters you want for your logo.",
     },
     {
-        type: 'input',
-        name: 'textColor',
-        message: 'What color do you want the logo text to be?'
+      type: "input",
+      name: "textColor",
+      message: "What color do you want the logo text to be?",
     },
     {
-        type: 'list',
-        name: 'logoShape',
-        choices: ['circle', 'triangle', 'square'],
-        message: 'What shape do you want your logo to be?'
+      type: "list",
+      name: "shape",
+      choices: ["circle", "triangle", "square"],
+      message: "What shape do you want your logo to be?",
     },
     {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What color do you want this shape to be? (this will be different from the text color!)'
+      type: "input",
+      name: "shapeColor",
+      message:
+        "What color do you want this shape to be? (this will be different from the text color!)",
     },
-])
-.then((response) => {
-    if (response.logoShape === "circle") {
-        shape = new Circle(response.logotext, response.textColor, response.shapeColor);
+  ])
+  .then((response) => {
+    let shape;
+    if (response.shape === "circle") {
+      shape = new Circle(
+        response.logotext,
+        response.textColor,
+        response.shapeColor
+      );
     }
-    if (response.logoShape === "triangle") {
-        shape = new Triangle(response.logotext, response.textColor, response.shapeColor);
+    if (response.shape === "triangle") {
+      shape = new Triangle(
+        response.logotext,
+        response.textColor,
+        response.shapeColor
+      );
     }
-    if (response.logoShape === "square") {
-        shape = new Square(response.logotext, response.textColor, response.shapeColor);
+    if (response.shape === "square") {
+      shape = new Square(
+        response.logotext,
+        response.textColor,
+        response.shapeColor
+      );
     }
-    fs.writeFile("logo.svg", shape, (err) => 
-        err ? console.log(err) : console.log("Initialization of Logo Generation is Complete. <3")
+    fs.writeFile("logo.svg", shape, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Initialization of Logo Generation is Complete. <3")
     );
-})
+  });
